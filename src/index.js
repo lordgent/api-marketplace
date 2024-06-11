a7aa55bcaa33de23608e1d2692f8b71d0a7fc99a
lordgent1 <lordgent@digitalcenter.id>
2024-06-11 21:02:11 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const route = require('./routes/index')
 const cors = require('cors')
+
+const PORT = process.env.PORT || 5000; 
 
 app.use(express.json());
 app.use(cors())
@@ -9,7 +12,6 @@ app.use(cors())
 app.use('/marketplace/api/v1/', route)
 app.use("/uploads", express.static("uploads"));
 
-const server = app.listen(5000, () => {
-    const port = server.address().port;
-    console.log(`Express is working on port ${port}`);
-  });
+app.listen(PORT,() => {
+    console.log(`server running on port ${PORT}}`)
+});
